145827c0340821d2e657ef2c6379675fecd0416c
mdb1710 <mdb1710@gmail.com>
2020-02-14 00:37:36 -0500

    completed basic form function

--- pages/investment/_id.vue
+++ pages/investment/_id.vue
@@ -3,25 +3,30 @@
     <h1 class="title">LOAN</h1>
     <p>Address: {{ investment.address }}</p>
     <p>Purpose: {{ investment.purpose }}</p>
+    <p>Id: {{ investment.id }}</p>
 
     <section class="investment_funds">
       <h3 class="subtitle">Loan Funds</h3>
       <ul>
         <!-- TODO: list all funds -->
+        <li v-for="fund in funds" :key="fund.id">
+          <span>{{ fund.amount }}</span>
+          <span>{{ fund.created_on }}</span>
+        </li>
       </ul>
     </section>
 
     <b-row>
       <b-form @submit="onSubmit">
-        <b-form-group label="fund Amount" label-for="fund-amount">
+        <b-form-group label="Fund Amount" label-for="amount">
           <b-input-group>
             <b-input-group-prepend>
               <span class="input-group-text">$</span>
             </b-input-group-prepend>
-            <b-form-input id="fund_amount" v-model="fund_amount" type="number" name="fund_amount" />
+            <b-form-input id="amount" v-model="amount" type="number" name="amount" />
           </b-input-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
         </b-form-group>
-        <b-button type="submit" variant="primary">Submit</b-button>
       </b-form>
     </b-row>
 
@@ -37,12 +42,29 @@ export default {
     let funds = await $axios.get(`/api/investment/${params.id}/funds`)
     return {
       investment: investment.data,
-      funds: funds.data
+      funds: funds.data,
+      amount: 0
     }
   },
   methods: {
     goBack() {
       this.$router.back()
+    },
+    async onSubmit(ev) {
+      ev.preventDefault()
+      let { investment, funds, amount } = this
+
+      let b = {
+        amount: this.amount,
+        investment_id: this.investment.id
+      }
+      console.log('fund submitted', b)
+      let funding = await this.$axios({
+        method: 'post',
+        url: '/api/funding',
+        data: b
+      })
+      console.log(funds)
     }
   }
 }
