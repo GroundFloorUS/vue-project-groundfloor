b9de42008dc3f1af3462f3014771ca3282e7c504
Benjamin Gray <bendoylegray@gmail.com>
2020-03-07 11:48:47 -0500

    Add styling to funding page

--- pages/funding.vue
+++ pages/funding.vue
@@ -2,18 +2,35 @@
   <b-container>
     <h2 class="title">FUNDING</h2>
 
-    <ul class="funding-list">
-      <li v-for="investment in funding" :key="investment.id">
-        <p>
-          <nuxt-link :to="'/investment/' + investment.id">
-            {{ investment.address }}
-          </nuxt-link>
-        </p>
-        <p>
-          {{ investment.loan_amount_dollars }}
-        </p>
-      </li>
-    </ul>
+    <table class="funding-list table table-striped">
+      <thead>
+        <tr>
+          <th>Address</th>
+          <th>Loan Amount</th>
+          <th>Rate</th>
+          <th>Term (mos)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="investment in funding" :key="investment.id">
+          <td>
+            <nuxt-link :to="'/investment/' + investment.id">
+              {{ investment.address }}
+            </nuxt-link>
+          </td>
+          <td>
+            ${{ investment.loan_amount_dollars }}
+          </td>
+          <td>
+            {{ investment.rate }}
+          </td>
+          <td>
+            {{ investment.expected_term_months }}
+          </td>
+        </tr>
+      </tbody>
+
+    </table>
   </b-container>
 </template>
 
