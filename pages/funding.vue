fc7ad0ce8dd0b1db378a2fc52f6fe743ae683b7d
John Misino <johnmisino@outlook.com>
2020-03-08 18:53:27 -0400

    Added styling and info to /funding.

--- pages/funding.vue
+++ pages/funding.vue
@@ -5,12 +5,22 @@
     <ul class="funding-list">
       <li v-for="investment in funding" :key="investment.id">
         <p>
-          <nuxt-link :to="'/investment/' + investment.id">
-            {{ investment.address }}
-          </nuxt-link>
+          Purpose: {{ investment.purpose }}
         </p>
         <p>
-          {{ investment.loan_amount_dollars }}
+          Address: {{ investment.address }} 
+        </p>
+        <p>
+          Rate: {{ investment.rate }}%
+        </p>
+        <p>
+          Expected Term: {{ investment.expected_term_months }} Months
+        </p>
+        <p>
+          Loan Amount (Dollars): ${{ investment.loan_amount_dollars }}&nbsp;
+          <b-button type="button" vairant="success" @click="invest(investment.id)">
+            Invest
+          </b-button>
         </p>
       </li>
     </ul>
@@ -24,6 +34,34 @@ export default {
     return {
       funding: funding.data
     }
+  },
+  methods: {
+    invest(id) {
+      document.location.href = '/investment/' + id
+    }
   }
 }
 </script>
+<style>
+.funding-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.funding-list > li {
+  font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+  border: 5px solid #ccc;
+  padding: 10px 10px 0px 10px;
+}
+
+.funding-list > li:last-child {
+  border-top: 0px;
+}
+
+.title {
+  font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
