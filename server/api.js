65626293883ce2de8e8b0cd6d0cc194de82092c3
Travis Franklin <hello@travislf.com>
2020-08-27 23:37:31 -0500

    change api methods, get funding, post funding

--- server/api.js
+++ server/api.js
@@ -5,7 +5,9 @@ let api = express.Router()
 
 api.get('/funding', (req, res, next) => {
   db.all(
-    `SELECT * FROM investment 
+    `SELECT i.*,
+       (SELECT SUM(f.amount) FROM funding f WHERE f.investment_id = i.id AND f.amount IS NOT NULL) funded
+    FROM investment i
        WHERE fully_funded = 0
        ORDER BY created_on DESC
     `,
@@ -43,7 +45,6 @@ api.get('/investment/:id', (req, res, next) => {
       if (err) {
         next(err)
       }
-
       res.json(row)
     }
   )
@@ -90,7 +91,7 @@ api.post('/investment', (req, res, next) => {
 
   db.serialize(() => {
     db.run(
-      `INSERT INTO investment 
+      `INSERT INTO investment
         (purpose, address, rate, expected_term_months, loan_amount_dollars)
        VALUES (?, ?, ?, ?, ?);
       `,
@@ -119,31 +120,71 @@ api.post('/investment', (req, res, next) => {
 api.post('/funding', (req, res, next) => {
   let { investment_id, amount } = req.body
 
-  db.serialize(() => {
-    db.run(
-      `INSERT INTO funding
-        (investment_id, amount)
-       VALUES (?, ?);
-      `,
-      [investment_id, amount],
-      err => {
-        if (err) {
-          next(err)
-        }
-        db.get(
-          `SELECT id, investment_id, amount, created_on
-           FROM funding WHERE rowid = ?`,
-          [this.lastID],
-          (err, row) => {
+  // get loan_amount_dollars
+  db.get(
+    `SELECT loan_amount_dollars
+     FROM investment WHERE id = ?`,
+    [investment_id],
+    (err, investment) => {
+      console.log('GOT INVESTMENT', investment)
+      if (err) {
+        next(err)
+      } else {
+        db.all(
+          `SELECT amount
+            FROM funding WHERE investment_id = ?`,
+          [investment_id],
+          (err, funds) => {
             if (err) {
               next(err)
+            } else {
+              const sumOfExistingFunds = funds.reduce(
+                (a, b) => a + (b['amount'] || 0),
+                0
+              )
+              const sumOfAllFunds =
+                parseInt(sumOfExistingFunds) + parseInt(amount)
+              const investmentLoanAmount = investment.loan_amount_dollars
+
+              if (sumOfAllFunds > investmentLoanAmount) {
+                const remainingAmount =
+                  parseInt(investmentLoanAmount) - parseInt(sumOfExistingFunds)
+                res.json({
+                  message: `Fund amount requested would overfund the loan. Choose an amount that is $${remainingAmount} or lower!`
+                })
+              } else {
+                db.serialize(() => {
+                  db.run(
+                    `INSERT INTO funding
+                      (investment_id, amount)
+                     VALUES (?, ?);
+                    `,
+                    [investment_id, amount],
+                    err => {
+                      if (err) {
+                        next(err)
+                      }
+                      db.get(
+                        `SELECT id, investment_id, amount, created_on
+                         FROM funding WHERE rowid = ?`,
+                        [this.lastID],
+                        (err, row) => {
+                          if (err) {
+                            next(err)
+                          }
+                          res.json(row)
+                        }
+                      )
+                    }
+                  )
+                })
+              }
             }
-            res.json(row)
           }
         )
       }
-    )
-  })
+    }
+  )
 })
 
 module.exports = api
