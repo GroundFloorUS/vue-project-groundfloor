40e55be5e30b0e66ef85a2e6a2c66a4a163ed6dd
Irwin Coleman <iwc@superboom.org>
2020-09-18 17:57:09 -0400

    Reformated funding page to use a table

--- pages/funding.vue
+++ pages/funding.vue
@@ -2,18 +2,21 @@
   <b-container>
     <h2 class="title">FUNDING</h2>
 
-    <ul class="funding-list">
-      <li v-for="investment in funding" :key="investment.id">
-        <p>
-          <nuxt-link :to="'/investment/' + investment.id">
-            {{ investment.address }}
+    <div>
+      <b-table :fields="fields" :items="funding" striped hover>
+        <template v-slot:cell(address)="data">
+          <nuxt-link :to="'/investment/' + data.item.id">
+            {{ data.item.address }}
           </nuxt-link>
-        </p>
-        <p>
-          {{ investment.loan_amount_dollars }}
-        </p>
-      </li>
-    </ul>
+        </template>
+        <template v-slot:cell(rate)="data">
+          {{ data.item.rate + '%' }}
+        </template>
+        <template v-slot:cell(expected_term_months)="data">
+          {{ data.item.expected_term_months + ' months' }}
+        </template>
+      </b-table>
+    </div>
   </b-container>
 </template>
 
@@ -22,7 +25,52 @@ export default {
   async asyncData({ $axios }) {
     let funding = await $axios.get('/api/funding')
     return {
-      funding: funding.data
+      funding: funding.data,
+      fields: [
+        'purpose',
+        'address',
+        'rate',
+        {
+          key: 'expected_term_months',
+          label: 'Expected Term'
+        },
+        {
+          key: 'loan_amount_dollars',
+          label: 'Loan Amount',
+          formatter: 'formatCurrency'
+        },
+        {
+          key: 'fully_funded',
+          label: 'Fully Funded?',
+          formatter: 'formatFunded'
+        },
+        {
+          key: 'created_on',
+          formatter: 'formatDate'
+        }
+      ]
+    }
+  },
+  methods: {
+    formatCurrency(value) {
+      const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0
+      })
+
+      return formatter.format(value)
+    },
+    formatFunded(value) {
+      return value == 1 ? 'Yes' : 'No'
+    },
+    formatDate(value) {
+      const date = new Date(value)
+      if (isNaN(date.getTime())) {
+        return ''
+      } else {
+        return date.toLocaleString()
+      }
     }
   }
 }
