2672ddc98214ea983b5f8670a0e88bb5ab2803db
Adam Taylor <adam.t.taylor@gmail.com>
2020-10-31 14:22:51 -0400

    Add Funded list
    - Similar to the funding list with loans set to fully funded.

--- pages/funded.vue
+++ pages/funded.vue
@@ -1,28 +1,37 @@
 <template>
   <b-container>
     <h2 class="title">FUNDED</h2>
-
-    <ul class="funded-list">
-      <li v-for="investment in funded" :key="investment.id">
-        <nuxt-link :to="'/investment/' + investment.id">
-          {{ investment.address }}
-        </nuxt-link>
-        <p>
-          {{ investment.loan_amount_dollars }}
-        </p>
-      </li>
-    </ul>
-
+    <div class="funding-list">
+      <FundItem v-for="investment in funded" :key="investment.id" :item="investment" />
+    </div>
   </b-container>
 </template>
 
 <script>
+import FundItem from './fundItem.vue'
 export default {
   async asyncData({ $axios }) {
     let funded = await $axios.get('/api/funded')
     return {
       funded: funded.data
     }
+  },
+  components: {
+    FundItem
   }
 }
 </script>
+<style scoped>
+h2 {
+  padding-bottom: 10px;
+}
+.funding-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.funding-list div {
+  width: 49%;
+  margin-bottom: 20px;
+}
+</style>
