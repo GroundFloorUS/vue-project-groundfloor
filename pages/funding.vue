69339979f45de216f78df64cee30b39ed552754b
Glenn Stovall <glennstovall@gmail.com>
2020-11-19 12:24:56 -0500

    Finishes initial styling pass of funding page.

--- pages/funding.vue
+++ pages/funding.vue
@@ -1,29 +1,30 @@
 <template>
   <b-container>
-    <h2 class="title">FUNDING</h2>
-    <b-card 
-      v-for="investment in funding" 
-      :key="investment.id"
-      :title="investment.address"
-      class="investment" >
-      <section class="investment_info">
-        <div class="cell">
-          <span>{{ investment.rate }}%</span>
-          <small>Rate</small>
-        </div>
-        <div class="cell">
-          <span>{{ investment.expected_term_months }}</span>
-          <small>Expected term months</small>
-        </div>
-        <div class="cell">
-          <span>${{ investment.loan_amount_dollars.toLocaleString() }}</span>
-          <small>Loan amount</small>
-        </div>
+    <h2 class="title mb-2">FUNDING</h2>
+    <section class="investment_container d-flex">
+      <b-card 
+        v-for="investment in funding" 
+        :key="investment.id"
+        :title="investment.address"
+        class="investment" >
+        <section class="investment_info mb-3 d-flex flex-row">
+          <div class="cell">
+            <span class="font-weight-bold">{{ investment.rate }}%</span>
+            <small>rate</small>
+          </div>
+          <div class="cell">
+            <span>{{ investment.expected_term_months }} mo.</span>
+            <small>expected term</small>
+          </div>
+          <div class="cell">
+            <span class="text-right">${{ investment.loan_amount_dollars.toLocaleString() }}</span>
+            <small>Loan amount</small>
+          </div>
+        </section>
+        <b-button :href="'/investment/' + investment.id" class="text-center" size="lg">View More Details</b-button>
+      </b-card>
+    </section>
 
-      </section>
-      <b-button href="'/investment/' + investment.id">View More Details</b-button>
-
-    </b-card>
   </b-container>
 </template>
 
@@ -40,13 +41,35 @@ export default {
 </script>
 
 <style scoped>
+.investment_container {
+  flex-wrap: wrap;
+}
+
+/* I debated on a two vs. three column layout here. I'm assuming that users want to peruse funding here, 
+   similar to something like Pinterest. I think three columns makes that a bit easier, but it also felt a bit cramped. 
+   */
 .investment {
   margin-bottom: 1em;
   box-shadow: inset 0 4px 0 #40a6cc;
+  width: 48%;
+  margin-right: 2%;
 }
 
 .investment .btn {
   background-color: #40a6cc;
   border: 0;
+}
+
+.investment_info {
+  justify-content: space-between;
+}
+
+.investment_info .cell {
+  display: flex;
+  flex-direction: column;
+}
+
+small {
+  color: #666;
 }
 </style>
