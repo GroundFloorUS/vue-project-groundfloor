8e8ba44f9eff867e3b0b9dc3442cdac5d4982ca4
Kevin Creel <kcreel@oxblue.com>
2021-08-10 21:36:26 -0400

    Adds form validation

--- pages/borrow.vue
+++ pages/borrow.vue
@@ -15,8 +15,14 @@
                     label-for="address">
         <b-form-input id="address"
                       v-model="address"
+                      :class="{'input-invalid': errors.address.inValid}"
                       type="text"
                       name="address" />
+        <span
+          v-show="errors.address.inValid"
+          class="error-msg"
+          v-text="errors.address.message"
+        />
       </b-form-group>
       
       <b-form-group label="Rate"
@@ -24,12 +30,18 @@
         <b-input-group>
           <b-form-input id="rate"
                         v-model="rate"
+                        :class="{'input-invalid': errors.rate.inValid}"
                         type="number"
                         name="rate"/>
           <b-input-group-append>
-            <span class="input-group-text">%</span>
+            <span :class="{'input-invalid': errors.rate.inValid}" class="input-group-text">%</span>
           </b-input-group-append>
         </b-input-group>
+        <span
+          v-show="errors.rate.inValid"
+          class="error-msg"
+          v-text="errors.rate.message"
+        />
       </b-form-group>
 
       <b-form-group label="Expected Term"
@@ -49,13 +61,19 @@
                     label-for="loan_amount_dollars">
         <b-input-group>
           <b-input-group-prepend>
-            <span class="input-group-text">$</span>
+            <span :class="{'input-invalid': errors.rate.inValid}" class="input-group-text">$</span>
           </b-input-group-prepend>
           <b-form-input id="loan_amount_dollars"
                         v-model="loan_amount_dollars"
+                        :class="{'input-invalid': errors.loan.inValid}"
                         type="number"
                         name="loan_amount_dollars"/>
         </b-input-group>
+        <span
+          v-show="errors.loan.inValid"
+          class="error-msg"
+          v-text="errors.loan.message"
+        />
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
@@ -81,12 +99,28 @@ export default {
       address: '',
       rate: 10,
       expected_term_months: 12,
-      loan_amount_dollars: 100000
+      loan_amount_dollars: 100000,
+      errors: {
+        address: {
+          inValid: false,
+          message: 'Address is required'
+        },
+        rate: {
+          inValid: false,
+          message: 'Rate must be above 5%'
+        },
+        loan: {
+          inValid: false,
+          message: 'Loan must be more than $50,000'
+        }
+      }
     }
   },
   methods: {
     async onSubmit(ev) {
       ev.preventDefault()
+      this.validateForm()
+      return
       let { address, rate, expected_term_months, loan_amount_dollars } = this
       let b = {
         purpose: this.purpose.value,
@@ -101,7 +135,21 @@ export default {
         data: b
       })
       this.$router.push({ path: `/funding` })
+    },
+    validateForm() {
+      this.errors.address.inValid = this.address === ''
+      this.errors.rate.inValid = this.rate <= 5
+      this.errors.loan.inValid = this.loan_amount_dollars <= 50000
     }
   }
 }
 </script>
+<style scoped>
+.input-invalid {
+  border-color: red;
+}
+.error-msg {
+  font-size: 0.75em;
+  color: red;
+}
+</style>
